0c02836d5c435f9ad0b4ee570b9839e27eacceda
louisa <louisa@MacBook-Air-109.local>
2022-09-19 15:20:25 -0400

    added ability to check variables and get mean hours of sleep.

--- data_load.R
+++ data_load.R
@@ -1,5 +1,10 @@
 #This script downloads the data.
 survey = read.csv("https://raw.githubusercontent.com/Intro-to-Data-Science-Template/intro_to_data_science_reader/main/content/class_worksheets/4_r_rstudio/data/survey_data.csv")
 
+#Check the variables
+str(survey)
+
+#Get the mean hours of sleep
+mean(survey$hours_sleep)
 
 write.csv(survey, "./survey_data.csv", row.names = FALSE)
